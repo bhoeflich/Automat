01d67cdc49fbc9a230c9cc8c548df774a0085e77
Bjoern Hoefer <hoefer.bjoern@gmail.com>
2019-12-05 10:42:19 +0100

    Initial Commit

---
+++ Automat/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Automat
+//
+//  Created by Björn Höfer on 05.12.19.
+//  Copyright © 2019 Björn Höfer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
